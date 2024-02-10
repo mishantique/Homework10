f782406dc557ff68774bdd8a96956804f8870485
mishantique <michvitko@mail.ru>
2024-02-10 20:14:38 +0300

    Добавьте файлы проекта.

---
+++ 10.1/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _10._1
+{
+    internal class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool IsCorrect = true;
+        public Angle(int gradus, int min, int sec)
+        {   
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        //1 angle = 360 gradus
+        //1 gradus = 60 min
+        // 1 min = 60 sec
+        public int Gradus
+        {   
+            get 
+            { 
+                return gradus; 
+            }
+            set
+            {   
+                if (value < 0)
+                {
+                    IsCorrect = false;
+                }
+                else
+                {
+                    gradus = value % 360;
+                }
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {   
+                if (value < 0)
+                {
+                    IsCorrect = false;
+                }
+                else 
+                {
+                    Gradus += value / 60;
+                    min += value % 60;
+                }
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    IsCorrect = false;
+                }
+                else
+                {
+                    Min += value / 60;
+                    sec += value % 60;
+                }
+            }
+        }
+
+        public float GetAngle()
+        {
+            return (float)gradus + (float)min /60 + (float)sec /(60*60);
+        }
+
+        public double GetRadians()
+        {   
+            float angle = GetAngle();
+            return (Math.PI) * angle / 180;
+        }
+    }
+}
---
+++ 10.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                Console.WriteLine("Введите градусы");
+                int gradus_ = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите минуты");
+                int min_ = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите секунды");
+                int sec_ = Convert.ToInt32(Console.ReadLine());
+
+                angle = new Angle(gradus_, min_, sec_);
+            } 
+            while (angle.IsCorrect == false);
+            Console.WriteLine("В переводе в радианы вы ввели: {0}", angle.GetRadians());
+            Console.ReadKey();
+        }
+    }
+}
